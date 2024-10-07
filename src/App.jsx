4c0b98aa1518de637b5c80a9d0302d34927d5ccd
vinayaka0305 <vav6473@gmail.com>
2024-10-07 16:55:39 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
+import React from "react";
+import Home from "./components/Home";
 import "./App.css";
-import MatterComponent from "./components/MatterComponent";
-import AboutMe from "./components/AboutMe";
-import HomeComponent from "./components/HomeComponent";
-import LatestWorks from "./components/LatestWorks";
 
 function App() {
-
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/latest" element={<LatestWorks/>}/>
-          <Route path="/aboutme" element={<AboutMe/>}/>
-        </Routes>
-      </Router>
+      <Home/>
     </>
   );
 }
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import AboutMe from "./AboutMe";
 
 const HomeComponent = () => {
   const sectionRef = useRef();
-  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const scrollSection = () => {
     if (sectionRef.current) {
@@ -11,10 +11,6 @@ const HomeComponent = () => {
       const scrollPosition = offsetTop - -620; // Scroll 100px above the section
       window.scrollTo({ top: scrollPosition, behavior: "smooth" });
     }
-  };
-
-  const handleAboutMeClick = () => {
-    navigate("/about-me"); // This will navigate to the "/about-me" route
   };
   return (
     <section className="flex flex-col justify-between h-screen">
@@ -172,33 +168,32 @@ const HomeComponent = () => {
             <p className="pointer-events-none font-['Merriweather'] italic my-4 md:my-8">
               Frontend Developer
             </p>
-            <Link to="/aboutme">
-              <button
-                className="bg-[#24baba] py-2 px-5 rounded font-extrabold bg-gradient-to-l from-[#059cc6] to-[#1DE5E5] relative hover:scale-110 ease-in-out duration-100 group"
-                // onClick={handleAboutMeClick}
+            <button
+              className="bg-[#24baba] py-2 px-5 rounded font-extrabold bg-gradient-to-l from-[#059cc6] to-[#1DE5E5] relative hover:scale-110 ease-in-out duration-100 group"
+              onClick={() => setIsOpen(true)}
+            >
+              About Me
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                x="0px"
+                y="0px"
+                width="44"
+                height="44"
+                viewBox="0 0 100 100"
+                className="absolute top-1/2 -translate-y-1/2 -right-6 group-hover:-right-8 ease-in-out duration-100"
               >
-                About Me
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  version="1.1"
-                  x="0px"
-                  y="0px"
-                  width="44"
-                  height="44"
-                  viewBox="0 0 100 100"
-                  className="absolute top-1/2 -translate-y-1/2 -right-6 group-hover:-right-8 ease-in-out duration-100"
-                >
-                  <g transform="translate(0,-952.36218)">
-                    <path
-                      d="m 88.999835,1002.3621 c 0,-0.4628 -0.2799,-1.0773 -0.5639,-1.3755 l -15.9997,-17.00026 c -0.747,-0.7723 -1.9572,-0.8618 -2.8281,-0.078 -0.7786,0.7007 -0.798,2.0673 -0.078,2.8282 l 12.8435,13.62516 -69.37347,0 c -1.1046,0 -2,0.8954 -2,2 0,1.1046 0.8954,2.0001 2,2.0001 l 69.37347,0 -12.8435,13.6252 c -0.7199,0.7608 -0.6688,2.0938 0.078,2.8281 0.7885,0.7752 2.0925,0.7062 2.8281,-0.078 l 15.9997,-17.0002 c 0.4701,-0.4611 0.556,-0.9052 0.5639,-1.3748 z"
-                      fill="#fff"
-                      stroke="white"
-                      stroke-width="2"
-                    />
-                  </g>
-                </svg>
-              </button>
-            </Link>
+                <g transform="translate(0,-952.36218)">
+                  <path
+                    d="m 88.999835,1002.3621 c 0,-0.4628 -0.2799,-1.0773 -0.5639,-1.3755 l -15.9997,-17.00026 c -0.747,-0.7723 -1.9572,-0.8618 -2.8281,-0.078 -0.7786,0.7007 -0.798,2.0673 -0.078,2.8282 l 12.8435,13.62516 -69.37347,0 c -1.1046,0 -2,0.8954 -2,2 0,1.1046 0.8954,2.0001 2,2.0001 l 69.37347,0 -12.8435,13.6252 c -0.7199,0.7608 -0.6688,2.0938 0.078,2.8281 0.7885,0.7752 2.0925,0.7062 2.8281,-0.078 l 15.9997,-17.0002 c 0.4701,-0.4611 0.556,-0.9052 0.5639,-1.3748 z"
+                    fill="#fff"
+                    stroke="white"
+                    stroke-width="2"
+                  />
+                </g>
+              </svg>
+            </button>
+            <AboutMe isOpen={isOpen} setIsOpen={setIsOpen} />
           </div>
         </div>
         <ul className="ml-auto space-y-6 text-[#b0b2c3] absolute right-8">
