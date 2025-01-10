ecfd90ecb21b45c5a3435e5147b8782a0c00e7b5
vinayaka0305 <vav6473@gmail.com>
2025-01-10 14:14:41 +0530

    code

--- src/components/LatestWorks.jsx
+++ src/components/LatestWorks.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import gaana from "../mockups/Ganna_Desktop.webp";
 import linkedin from "../mockups/linkedin.png";
+import calculator from "../mockups/cal.png";
+import game from "../mockups/game_tic.png";
 
 const LatestWorks = () => {
   return (
@@ -9,10 +11,11 @@ const LatestWorks = () => {
         latest works
       </h2>
       {/* projects*/}
+
       {/*project gaana*/}
       <div class="flex flex-col sm:flex-row gap-4 sm:gap-['80px'] items-center mt-12 sm:mt-8 relative">
         {/* connecting line */}
-        <div className="h-[1px] left-1/4 right-1/2 bg-[#0077b6] absolute top-1/2 hidden sm:block"></div>
+        <div className="h-[1px] left-1/4 right-1/2 bg-[#df3409] absolute top-1/2 hidden sm:block"></div>
         {/* connecting-dot */}
         <div className="w-4 h-4 rounded-full border-[3px] border-[#df3409] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-125 ease-in-out duration-100 hidden sm:block"></div>
         <a
@@ -24,7 +27,7 @@ const LatestWorks = () => {
             <img
               className="max-w-[400px] w-full mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)]"
               src={gaana}
-              alt=""
+              alt="gaana"
             />
 
             <span className="flex group-hover:-top-14 ease-jump duration-200 bg-[#df3409] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block sm:after:items-center after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
@@ -80,6 +83,7 @@ const LatestWorks = () => {
           </ul>
         </div>
       </div>
+
       {/*project linkedin*/}
       <div class="flex flex-col sm:flex-row gap-4 sm:gap-['80px'] items-center mt-12 sm:mt-16 relative">
         <div class="w-full">
@@ -114,7 +118,7 @@ const LatestWorks = () => {
           </ul>
         </div>
         {/* connecting line */}
-        <div className="h-[1px] left-1/2 right-1/4 bg-[#0077b6] absolute top-1/2 hidden sm:block"></div>
+        <div className="h-[1px] left-1/2 right-1/4 bg-[#0f69c3] absolute top-1/2 hidden sm:block"></div>
         {/* connecting-dot */}
         <div className="w-4 h-4 rounded-full border-[3px] border-[#0f69c3] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-125 ease-in-out duration-100 hidden sm:block"></div>
         <a
@@ -126,7 +130,7 @@ const LatestWorks = () => {
             <img
               className="max-w-[400px] relative w-full z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.8)]"
               src={linkedin}
-              alt=""
+              alt="linkedin"
             />
 
             <span className="flex group-hover:-top-14 ease-jump duration-200 text-black bg-[#0f69c3] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
@@ -151,7 +155,136 @@ const LatestWorks = () => {
           </div>
         </a>
       </div>
+
       {/* another project div */}
+
+      {/*project Calculator*/}
+      <div class="flex flex-col sm:flex-row gap-4 sm:gap-['80px'] items-center mt-14 sm:mt-16 relative">
+        {/* connecting line */}
+        <div className="h-[1px] left-1/4 right-1/2 bg-[#a8d7db] absolute top-1/2 hidden sm:block"></div>
+        {/* connecting-dot */}
+        <div className="w-4 h-4 rounded-full border-[3px] border-[#a8d7db] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-125 ease-in-out duration-100 hidden sm:block"></div>
+        <a
+          href="https://calculatorbasic.onrender.com/"
+          target="_blank"
+          className="flex w-full relative justify-center sm:justify-start"
+        >
+          <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200">
+            <img
+              className="max-w-[400px] w-full mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.6)]"
+              src={calculator}
+              alt="calculator"
+            />
+
+            <span className="flex group-hover:-top-14 ease-jump duration-200 bg-[#9aa6e0] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block sm:after:items-center after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+              basic_calculator
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+          </div>
+        </a>
+        <div class="w-full">
+          <h3 class="text-[#9aa6e0] font-bold text-2xl md:text-4xl">
+            {" "}
+            Basic Calculator
+          </h3>
+          {/* <span class="text-[#9aa6e0] text-base md:text-lg">(music)</span> */}
+          <p class="text-justify text-sm md:text-base mt-2">
+            This calculator was developed using HTML, CSS, and JavaScript,
+            offering users a straightforward tool to perform everyday
+            calculations with a responsive and visually appealing design.
+          </p>
+
+          <ul class="flex flex-wrap gap-2 mt-2">
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # html
+            </li>
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # css
+            </li>
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # javascript
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/*project tic_tac_toe*/}
+      <div class="flex flex-col sm:flex-row gap-4 sm:gap-['80px'] items-center mt-12 sm:mt-16 relative">
+        <div class="w-full">
+          <h3 class="text-[#ffe400] font-bold text-2xl md:text-4xl">
+            tic_tac_toe
+          </h3>
+          <span class="text-[#ffe400] text-base md:text-lg">(game)</span>
+          <p class="text-justify text-sm md:text-base mt-2">
+          This interactive Tictactoe game was developed using HTML, CSS, and JavaScript, offering a fun and simple
+          way for users to play the classic game directly in the browser.
+          </p>
+
+          <ul class="flex flex-wrap gap-2 mt-2">
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # html
+            </li>
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # css
+            </li>
+            <li class="border rounded-[50px] border-[#999] px-[10px] py-[5px]">
+              # javascript
+            </li>
+          </ul>
+        </div>
+        {/* connecting line */}
+        <div className="h-[1px] left-1/2 right-1/4 bg-[#ffe400] absolute top-1/2 hidden sm:block"></div>
+        {/* connecting-dot */}
+        <div className="w-4 h-4 rounded-full border-[3px] border-[#ffe400] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-125 ease-in-out duration-100 hidden sm:block"></div>
+        <a
+          href="https://tic-tac-toe-basicgame.onrender.com/"
+          target="_blank"
+          className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start"
+        >
+          <div className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200 sm:ml-auto">
+            <img
+              className="max-w-[400px] relative w-full z-10 drop-shadow-[0_0px_60px_rgba(59,130,246,0.8)]"
+              src={game}
+              alt="game"
+            />
+
+            <span className="flex group-hover:-top-14 ease-jump duration-200 text-black bg-[#ffe400] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2">
+             tic_tac_toe
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+          </div>
+        </a>
+      </div>
 
       {/* center-line section*/}
       <div className="absolute w-[2px] bg-[#0077b6] top-0 bottom-0 left-1/2 -translate-x-1/2 hidden sm:block"></div>
