97f0c079b7fd18f04887f529276f7b00d826d84a
vinayaka0305 <vav6473@gmail.com>
2025-03-06 22:36:07 +0530

    code

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -15,6 +15,7 @@ const HomeComponent = () => {
   return (
     <section className="flex flex-col justify-between h-screen">
       <header className="pointer-events-none z-10 flex items-center justify-between mx-4 mt-4 max-w-screen-2xl forced-color-adjust-auto">
+        {/* small Logo */}
         <svg
           width="60"
           height="60"
@@ -31,22 +32,13 @@ const HomeComponent = () => {
             />
           </g>
         </svg>
+        {/* small Logo */}
         <nav className="flex pointer-events-auto gap-4 mx-1 cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            className="hidden"
+          <a
+            href="tel:8749018405"
+            className="transition-transform duration-300 ease-in-out hover:scale-150 hover:rotate-6 hover:opacity-80 hover:text-blue-500"
           >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-          </svg>
-          <a href="tel:8749018405">
+            {/* telephone logo */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -61,11 +53,14 @@ const HomeComponent = () => {
             >
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
             </svg>
+            {/* telephone logo */}
           </a>
           <a
             href="https://wa.me/8749018405?text=Hello! I'm from ur portfolio....."
             target="_blank"
+            className="transition-transform duration-300 ease-in-out hover:scale-150 hover:rotate-6 hover:opacity-80 hover:text-[#00d757]"
           >
+            {/* whatsapp logo */}
             <svg
               aria-hidden="true"
               focusable="false"
@@ -79,15 +74,17 @@ const HomeComponent = () => {
               viewBox="0 0 448 512"
             >
               <path
-                fill="white"
+                fill="currentColor"
                 d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"
               ></path>
             </svg>
+            {/* whatsapp logo */}
           </a>
         </nav>
       </header>
       <div className="flex -mt-10 relative z-10">
         <div className="w-full px-4 max-w-screen-xl mx-auto">
+          {/* v logo */}
           <svg
             viewBox="0 0 255 279"
             fill="none"
@@ -161,6 +158,7 @@ const HomeComponent = () => {
               </filter>
             </defs>
           </svg>
+          {/* v logo */}
           <div className="relative ml-4 sm:ml-24 md:ml-28">
             <h1 className="pointer-events-none text-4xl md:text-[64px] font-['Spartan']">
               Vinayaka
@@ -198,12 +196,14 @@ const HomeComponent = () => {
         </div>
         <ul className="ml-auto space-y-6 text-[#b0b2c3] absolute right-8">
           <li>
+             {/* linkedin logo */}
             <a
               href="https://www.linkedin.com/in/vinayaka-av-03-05m/"
               target="_blank"
+              
             >
               <svg
-                className="w-7 hover:text-white"
+                className="w-7 hover:text-[#0a66c2] transition-transform duration-300 ease-in-out hover:scale-150 hover:opacity-80"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 448 512"
               >
@@ -213,14 +213,16 @@ const HomeComponent = () => {
                 />
               </svg>
             </a>
+             {/* linkedin logo */}
           </li>
           <li>
+            {/* x logo */}
             <a
               href="https://x.com/BheeshmaA18212?t=u6Yb7agGwxWZdMe0x4aiqA&s=09"
               target="_blank"
             >
               <svg
-                className="w-7 hover:text-white"
+                 className="w-7 hover:text-white transition-transform duration-300 ease-in-out hover:scale-150 hover:opacity-80"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
               >
@@ -230,11 +232,13 @@ const HomeComponent = () => {
                 />
               </svg>
             </a>
+             {/* x logo */}
           </li>
           <li>
+             {/* email logo */}
             <a href="mailto:vav6473@gmail.com">
               <svg
-                className="w-7 hover:text-white"
+                className="w-7 hover:text-red-500 transition-transform duration-300 ease-in-out hover:scale-150 hover:opacity-80"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
               >
@@ -244,11 +248,13 @@ const HomeComponent = () => {
                 />
               </svg>
             </a>
+             {/* email logo */}
           </li>
           <li>
+            {/* github logo */}
             <a href="https://github.com/vinayaka0305" target="_blank">
               <svg
-                className="w-7 hover:text-white"
+                 className="w-7 hover:text-white transition-transform duration-300 ease-in-out hover:scale-150 hover:opacity-80"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 496 512"
               >
@@ -258,6 +264,7 @@ const HomeComponent = () => {
                 />
               </svg>
             </a>
+             {/* github logo */}
           </li>
         </ul>
       </div>
